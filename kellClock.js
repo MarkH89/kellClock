ce132656cdf3419d8cc8e3963655d23e9433186f
Markus <markholden1989@googlemail.com>
2018-12-08 15:08:44 +0000

    Return to original Prototype pattern

--- kellClock.js
+++ kellClock.js
@@ -1,141 +1,142 @@
-var kellClock = (function () {
-
-    var _run = function () {
-        // Check clock is ready to go
-        if (!_data._initialised || !_data._readyToRun) {
-            console.log('Clock not initialised');
-            return;
-        }
-        // Insert time fracts
-        for (let k in _data._fracts) {
-            if (k == 'd' && _data._daysAsHours) {
-                // If Days is to be included as hours, then ignore the D element
-                continue;
-            }
-            _data._container.appendChild(_data._fracts[k]);
-            // Check text contents exists and insert if required
-            if (_data._textContents && (Object.keys(_data._textContents).length !== 0) && (_data._textContents.constructor === Object)) {
-                let e = document.createElement('span');
-                e.className = 'label';
-                e.textContent = _data._textContents[k];
-                _data._container.appendChild(e);
-            }
-        }
-        // Begin the countdown
-        _data._timeNow = new Date().getTime();
-        _data._timeLeft = _data._dateEnd - _data._timeNow;
-        if (_data.timeLeft < 0) {
-            _data.selector.textContent = 'EXPIRED';
-        } else {
-            _update();
-        }
-        _data._timerSet = 1;
-    }
-
-    var _update = function () {
-        _data._timeNow = new Date().getTime();
-        _data.timeLeft = _data._dateEnd - _data._timeNow;
-        _data._container.dataset.clockStatus = "running";
-        if (!_data._daysAsHours) {
-            _data._fracts.d.textContent = _stringPad(Math.floor(_data.timeLeft / _data._d));
-        }
-        _data._fracts.h.textContent = _data._daysAsHours ? _stringPad(Math.floor((_data.timeLeft % _data._d) / _data._h) + Math.floor(_data.timeLeft / _data._d) * 24) : _stringPad(Math.floor((_data.timeLeft % _data._d) / _data._h));
-        _data._fracts.m.textContent = _stringPad(Math.floor((_data.timeLeft % _data._h) / _data._m));
-        _data._fracts.s.textContent = _stringPad(Math.floor((_data.timeLeft % _data._m) / 1000));
-        if (_data.timeLeft < 0) {
-            _data._selector.textContent = 'EXPIRED';
-            _data._container.dataset.clockStatus = "expired";
-        } else {
-            window.setTimeout(() => { _update(); }, 1000);
-        }
-    }
-
-    var _init = function (selector, dateEndString, daysAsHours = false, minPadding = 2, textContents = {
+function kellClock(
+    selector,
+    dateEndString,
+    daysAsHours = false,
+    minPadding = 2,
+    textContents = {
         d: 'd',
         h: 'h',
         m: 'm',
         s: 's'
-    }) {
-        // Check required values have been passed
-        if (!selector || !dateEndString) { return null; }
-
-        // Set Initial Data
-        _data._selector = selector;
-        _data._dateEndString = dateEndString;
-        _data._daysAsHours = daysAsHours;
-        _data._initialised = false;
-        _data._textContents = textContents;
-        _data._minPadding = minPadding;
-
-        _data._initialised = true;
-
-        // Preflight setup
-        _data._readyToRun = _preflight() ? true : false;
-
-        return this;
     }
+) {
+    // Check required values have been passed
+    if (!selector || !dateEndString) { return null; }
 
-    var _preflight = function () {
-        // Preflight checks and initialisations
-        // Check selector exists
-        if ((_data._container = document.querySelector(_data._selector)) == null) {
-            console.log('No container for clock');
-            return false;
+    this._data = (function () {
+        return {
+            _selector: null,
+            _initialised: false,
+            _daysAsHours: false,
+            _textContents: {},
+            _dateEndString: '',
+            _dateEnd: 0,
+            _container: null,
+            _timeLeft: 0,
+            _timeNow: 0,
+            _d: 1000 * 60 * 60 * 24,
+            _h: 1000 * 60 * 60,
+            _m: 1000 * 60,
+            _minPadding: 0,
+            _fracts: {
+                d: null,
+                h: null,
+                m: null,
+                s: null
+            }
         }
-        // Check selecotr is not already in use
-        if (_data._container.dataset.clockStatus != undefined) {
-            console.log("Clock already init'd!");
-            return false;
-        }
-        // Check DateEnd is a valid time
-        _data._dateEnd = new Date(_data._dateEndString).getTime();
-        if (_data._dateEnd == null || _data._dateEnd == 0 || _data._dateEnd == undefined || isNaN(_data._dateEnd)) {
-            console.log('Failed to find end date');
-            return false;
-        }
-        // Set data on element for clock
-        _data._container.dataset.clockStatus = "init";
-        // Create time fracts
-        for (let k of Object.keys(_data._fracts)) {
-            _data._fracts[k] = document.createElement('span');
-            _data._fracts[k].className = `${_data._selector}__${k} kellClock-fraction`;
-        }
-        // All checks and presets completed. Ready to run
-        return true;
+    })();
+
+    // Set Initial Data
+    this._data._selector = selector;
+    this._data._dateEndString = dateEndString;
+    this._data._daysAsHours = daysAsHours;
+    this._data._initialised = false;
+    this._data._textContents = textContents;
+    this._data._minPadding = minPadding;
+
+    // Preflight setup
+    this._data._initialised = this._preflight();
+
+    return this;
+};
+
+kellClock.prototype._preflight = function () {
+    if (this._data._initialised) { return true; }
+    // Preflight checks and initialisations
+    // Check selector exists
+    if ((this._data._container = document.querySelector(this._data._selector)) == null) {
+        console.log('No container for clock');
+        return false;
     }
+    // Check selecotr is not already in use
+    if (this._data._container.dataset.clockStatus != undefined) {
+        console.log("Clock already init'd!");
+        return false;
+    }
+    // Check DateEnd is a valid time
+    this._data._dateEnd = new Date(this._data._dateEndString).getTime();
+    if (this._data._dateEnd == null || this._data._dateEnd == 0 || this._data._dateEnd == undefined || isNaN(this._data._dateEnd)) {
+        console.log('Failed to find end date');
+        return false;
+    }
+    // Set data on element for clock
+    this._data._container.dataset.clockStatus = "init";
+    // Create time fracts
+    for (let k of Object.keys(this._data._fracts)) {
+        this._data._fracts[k] = document.createElement('span');
+        this._data._fracts[k].className = `${this._data._selector}__${k} kellClock-fraction`;
+    }
+    // All checks and presets completed. Ready to run
+    return true;
+}
 
-    var _data = {
-        _selector: null,
-        _initialised: false,
-        _readyToRun: false,
-        _daysAsHours: false,
-        _textContents: {},
-        _dateEndString: '',
-        _dateEnd: 0,
-        _container: null,
-        _timeLeft: 0,
-        _timeNow: 0,
-        _d: 1000 * 60 * 60 * 24,
-        _h: 1000 * 60 * 60,
-        _m: 1000 * 60,
-        _timerSet: 0,
-        _minPadding: 0,
-        _fracts: {
-            d: null,
-            h: null,
-            m: null,
-            s: null
+kellClock.prototype.run = function () {
+    // Check clock is ready to go
+    if (!this._data._initialised) {
+        console.log('Clock not initialised');
+        return;
+    }
+    // Insert time fracts
+    for (let k in this._data._fracts) {
+        if (k == 'd' && this._data._daysAsHours) {
+            // If Days is to be included as hours, then ignore the D element
+            continue;
+        }
+        this._data._container.appendChild(this._data._fracts[k]);
+        // Check text contents exists and insert if required
+        if (this._data._textContents && (Object.keys(this._data._textContents).length !== 0) && (this._data._textContents.constructor === Object)) {
+            let e = document.createElement('span');
+            e.className = 'label';
+            e.textContent = this._data._textContents[k];
+            this._data._container.appendChild(e);
         }
     }
-
-    // Improved String Padding
-    var _stringPad = function (n) {
-        return (String(n).split("").length >= _data._minPadding) ? String(n) : String("0" + n).slice(-(_data._minPadding));
+    // Begin the countdown
+    this._data._timeNow = new Date().getTime();
+    this._data._timeLeft = this._data._dateEnd - this._data._timeNow;
+    // Check there's time left to go, otherwise, don't run the update!
+    if (this._data.timeLeft < 0) {
+        this._data.selector.textContent = 'EXPIRED';
+    } else {
+        this._update();
     }
+}
 
-    return {
-        run: _run,
-        init: _init
+kellClock.prototype._update = function () {
+    // Calculate time remaining
+    this._data._timeNow = new Date().getTime();
+    this._data.timeLeft = this._data._dateEnd - this._data._timeNow;
+    // If timer has expired...
+    if (this._data.timeLeft < 0) {
+        this._data._selector.textContent = 'EXPIRED';
+        this._data._container.dataset.clockStatus = "expired";
+    } else {
+        // Set status to Running
+        this._data._container.dataset.clockStatus = "running";
+        // Only update Days if not daysAsHours
+        if (!this._data._daysAsHours) {
+            this._data._fracts.d.textContent = this._stringPad(Math.floor(this._data.timeLeft / this._data._d));
+        }
+        // Update text content for the rest
+        this._data._fracts.h.textContent = this._data._daysAsHours ? this._stringPad(Math.floor((this._data.timeLeft % this._data._d) / this._data._h) + Math.floor(this._data.timeLeft / this._data._d) * 24) : _stringPad(Math.floor((this._data.timeLeft % this._data._d) / this._data._h));
+        this._data._fracts.m.textContent = this._stringPad(Math.floor((this._data.timeLeft % this._data._h) / this._data._m));
+        this._data._fracts.s.textContent = this._stringPad(Math.floor((this._data.timeLeft % this._data._m) / 1000));
+        // Set timeout to run for the next second
+        window.setTimeout(() => { this._update(); }, 1000);
     }
+}
 
-})();
+kellClock.prototype._stringPad = function (n) {
+    return (String(n).split("").length >= this._data._minPadding) ? String(n) : String("0" + n).slice(-(this._data._minPadding));
+}
