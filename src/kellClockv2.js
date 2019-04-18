425f8ee660cb09e578fb182144f2b1b06c90e6ef
dev <dev@C02NR3MJF8J4.local>
2019-04-18 16:55:58 +0100

    v2 functions added and updated

--- src/kellClockv2.js
+++ src/kellClockv2.js
@@ -7,13 +7,20 @@ Version 2.0.0
 // Check if kellClock already exists, and return new instance if not
 var kellClock = kellClock || (function(){
 
-    var _data = {
+    const _data = {
         _clockObjects: [],
         _day: 1000 * 60 * 60 * 24,
         _hour: 1000 * 60 * 60,
         _month: 1000 * 60,
         _debug: false,
         _initComplete: false
+    }
+
+    const _clockStatus = {
+        INITIALISED: 'Initialised',
+        RUNNING: 'Running',
+        PAUSED: 'Paused',
+        DEREGISTERING: 'Deregister'
     }
 
     /*
@@ -32,22 +39,72 @@ var kellClock = kellClock || (function(){
     }
 
     function _init(debug = false) {
-        // Do stuff
+        if(_data._initComplete) return;
+        // * Do stuff
         _data._initComplete = true;
+        return;
     }
 
-    function _findID(clockID) {
-        // Find id
+    function _findIndexByID(clockID) {
+        // * Find id
         return _data._clockObjects.findIndex(function(el){
             return el.ID == clockID;
         });
     }
 
-    function _createClock(selector, clockID, timeEnd) {
-        if(_findID(clockID) != -1) {
-            _log("ID already exists");
-            return;
+    function _findSelector(selector) {
+        // Check selector
+        let selector = document.querySelector(selector);
+        if(!selector) {
+            _log("Selector not found");
+            return null;
         }
+        // * Check selector is not already in use
+        var i = _data._clockObjects.findIndex(function(el) {
+            return el.selector == selector;
+        });
+        if(i != -1) {
+            _log("Selector is already in use")
+            return null;
+        }
+
+        return selector;
+    }
+
+    function _getTime(time) {
+        let time = new Date(time);
+        if(time == null || time == 0 || time == undefined || isNaN(time)) {
+            _log("Failed to create time from time string");
+            return null;
+        }
+        return time;
+    }
+
+    function _createClock(selector, clockID, timeEnd) {
+        // * Check ID
+        if(_findIndexByID(clockID) != -1) {
+            _log("ID already exists");
+            return null;
+        }
+        // * Check Selector
+        selector = _findSelector(selector);
+        if(!selector) {
+            _log("Failed to use selector");
+            return null;
+        }
+        // * Check timeend
+        timeEnd = _getTime(timeEnd);
+        if(!timeEnd) {
+            _log("Failed to create time");
+            return null;
+        }
+        // * All should now be well, so add to the list of objects
+        return {
+            ID: clockID,
+            selector: selector,
+            timeEnd: timeEnd,
+            status: _clockStatus.INITIALISED
+        };
     }
 
     function _register(selector, clockID, timeEnd) {
@@ -60,14 +117,31 @@ var kellClock = kellClock || (function(){
             _log("Failed to create new clock");
             return;
         }
-        // Call and return _createClock()
+        // Add clock to the list of registered objects
+        _data._clockObjects.push(clock);
     }
 
-    function _deregister(id) {}
+    function _deregister(id) {
+        // Find clock index to remove
+        let clockIndex = _findIndexByID(id);
+        if(clockIndex != -1) {
+            // Removal to occur on update
+            _data._clockObjects[clockIndex].status = _clockStatus.DEREGISTERING;
+        } else {
+            _log("Clock to deregister not found");
+        }
+    }
 
-    function _pause(id = null) {}
+    function _pauseByID(id = null) {
+        let clockIndex = _findIndexByID(id);
+        if(clockIndex != -1) {
+            _data._clockObjects[clockIndex].status = _clockStatus.PAUSED;
+        } else {
+            _log("Clock to pause not found");
+        }
+    }
 
-    function _run(id = null) {}
+    function _runByID(id = null) {}
 
     function _update() {}
 
@@ -75,8 +149,8 @@ var kellClock = kellClock || (function(){
         init: _init,
         register: _register,
         deregister: _deregister,
-        pause: _pause,
-        run: _run
+        pause: _pauseByID,
+        run: _runByID
     }
 
 })();
